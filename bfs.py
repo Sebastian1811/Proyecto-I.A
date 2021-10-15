d55e22724cb227e2346bdbf0f1206485c62ebc76
Sebastian <eljhoan1411@gmail.com>
2021-10-15 13:21:26 -0500

    algoritmo costo uniforme

--- bfs.py
+++ bfs.py
@@ -1,5 +1,6 @@
 from collections import deque
 import txt
+import queue
 cola = deque()
 colaDibujo = deque()
 mapa= list()
@@ -42,6 +43,11 @@ def findStart():
                 x = i
                 y = j
     return x,y
+def refactorMapa():
+    for i in range(len(mapa)):
+        for j in range(len(mapa[0])):
+            if mapa[i][j] == "2":
+                mapa[i][j] = "1"
 def bfs():
     visitados = list()
     makemapa()
@@ -61,4 +67,30 @@ def bfs():
                 tupla = hijos[i]
                 if int(mapa[tupla[0]][tupla[1]]) != 0: # no deberia mirar muros xd
                     cola.append(hijos[i]) #hijos en cola
-#bfs()
+def costoUniforme():
+    cola = queue.PriorityQueue()
+    visitados = list()
+    makemapa()
+    x,y = findStart()
+    hijos =list()
+    cola.put((0,[x,y]))
+    while cola is not cola.empty():
+        expandL = list(cola.get())
+        expand = expandL[1]
+        colaDibujo.append(expand)
+        visitados.append(expand)
+        if int(mapa[expand[0]][expand[1]]) == 4: # es meta?
+            print(expand[0]," ",expand[1])
+            break
+        if int(mapa[expand[0]][expand[1]]) == 3:
+            refactorMapa()
+        hijos = hijosNodo([expand[0],expand[1]]) # creo los hijos
+        for i in range(len(hijos)):
+            if hijos[i] not in visitados: #no me devuelvo
+                tupla = hijos[i]
+                if int(mapa[tupla[0]][tupla[1]]) != 0 and int(mapa[expand[0]][expand[1]]) == 3:
+                    cola.put((0,hijos[i]))
+                if int(mapa[tupla[0]][tupla[1]]) != 0: # no deberia mirar muros xd
+                    cola.put((int(mapa[tupla[0]][tupla[1]]),hijos[i])) #hijos en cola
+        print(visitados)
+costoUniforme()
