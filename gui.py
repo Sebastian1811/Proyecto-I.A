8bd4a97d3e6e1aa302972894eb6dd8fab51ae726
Brayan Cortes <cortes.brayan@correounivalle.edu.co>
2021-10-14 22:39:24 -0500

    Organizar mapa

--- gui.py
+++ gui.py
@@ -1,20 +1,51 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 1000, 1000
-black = 0, 0, 0
+size = width, height = 1720, 900
+white = 255, 255, 255
+red = (255,0,0)
+x=0
+y=0
 
 screen = pygame.display.set_mode(size)
 
+mapa = [
+    "XXXXXXX",
+    "X     X",
+    "X X   X",
+    "X  XX X",
+    "X0    X",
+    "XXXXXXX"
+
+]
 
 Mononoke = pygame.image.load("Among u s.png")
-velocidad_amongus = 0
+Aparicion = (120,480)
 
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-            
-    screen.fill(black)
-    screen.blit(Mononoke, (500,500))
+
+    #pintar mapa
+    screen.fill(white)
+    x=0
+    y=0
+    for fila in mapa:
+        for  muro in fila:
+            if muro == "X":
+                pygame.draw.rect(screen,red,[x,y,120,120],0)
+            x+=121
+        x=0
+        y+=121
+    
+    x=0
+    y=0
+    for fila in mapa:
+        for muro in fila:
+            if muro == "0":
+                screen.blit(Mononoke,Aparicion)
+
+
+
     pygame.display.flip()
