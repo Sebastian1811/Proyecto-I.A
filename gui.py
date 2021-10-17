2f3ed53375809e5976771b4c3b8e25629c1d19e6
Brayan Cortes <cortes.brayan@correounivalle.edu.co>
2021-10-16 21:32:13 -0500

    MONONOKE MOVI

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import sys, pygame
+import time
 import bfs
 pygame.init()
 
@@ -8,8 +9,43 @@ red = (255,0,0)
 black = (0,0,0)
 x=0
 y=0
+screen = pygame.display.set_mode(size,pygame.RESIZABLE)
+#bfs.makemapa()
+mapa = bfs.mapa
+camino = bfs.returnPath(1)
+Mononoke = pygame.image.load("mononoke.png")
+Mononoke = pygame.transform.scale(Mononoke, (100, 100))
+Ciervo = pygame.image.load("ciervo.png")
+Ciervo = pygame.transform.scale(Ciervo,(100,100))
+Elemental = pygame.image.load("elementales.png")
+Elemental = pygame.transform.scale(Elemental,(100,100))
+dios_Ciervo = pygame.image.load("dios_ciervo.png")
+dios_Ciervo = pygame.transform.scale(dios_Ciervo,(100,100))
+count = 0
+Xamongus = 0
+Yamongus = 0
 
-def Movimientos():
+
+def Guiar_Mononoke(camino):
+    global Xamongus
+    global Yamongus
+    pintarmapa()
+    for i in range (len(camino)):
+        x1 = camino[i][0]
+        y1 = camino[i][1]
+        mapa[Xamongus][Yamongus] = "1"
+        mapa[x1][y1] ="5"
+        Xamongus = x1
+        Yamongus = y1
+        pintarmapa()
+        time.sleep(5)
+
+         
+
+            
+
+
+"""def Movimientos():
     if event.type == pygame.KEYDOWN:
         try:
             contenido = mapa[(int(Xamongus)-1)][int(Yamongus)]
@@ -32,29 +68,13 @@ def Movimientos():
                         mapa[int(Xamongus)][int(Yamongus)]="1"
                         mapa[int(Xamongus)][int(Yamongus)+1]="5"
         except Exception as inst:
-            print("amongus no tiene mas espacio")
-   
+            print("amongus no tiene mas espacio")"""
 
-screen = pygame.display.set_mode(size,pygame.RESIZABLE)
-bfs.makemapa()
-mapa = bfs.mapa
-Mononoke = pygame.image.load("mononoke.png")
-Mononoke = pygame.transform.scale(Mononoke, (100, 100))
-Ciervo = pygame.image.load("ciervo.png")
-Ciervo = pygame.transform.scale(Ciervo,(100,100))
-Elemental = pygame.image.load("elementales.png")
-Elemental = pygame.transform.scale(Elemental,(100,100))
-dios_Ciervo = pygame.image.load("dios_ciervo.png")
-dios_Ciervo = pygame.transform.scale(dios_Ciervo,(100,100))
-
-count = 0
-Xamongus = 0
-Yamongus = 0
-while 1:
-    screen.fill(black)
+def pintarmapa():
     x=0
     y=0
-
+    global Xamongus
+    global Yamongus
     for fila in range(len(mapa)):
         for  columna in range(len(mapa[0])):
             if mapa[fila][columna] == "0":
@@ -79,10 +99,22 @@ while 1:
                 x+=121
         x=0
         y+=121
+
+
+while 1:
+    screen.fill(black)
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-        Movimientos()
+    #input()
+    if Xamongus != 0 and Yamongus !=4:
+        Guiar_Mononoke(camino)
+        print(mapa)
+        print(Xamongus,",",Yamongus)
+    pintarmapa()
+    
+    #Movimientos()
 
 
 
