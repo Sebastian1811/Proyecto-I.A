6a31204606e3659e20f3b25b02bd760ba3fc0bc5
Brayan Cortes <cortes.brayan@correounivalle.edu.co>
2021-10-16 23:18:51 -0500

    Mensajes de algoritmos

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ import time
 import bfs
 pygame.init()
 
-size = width, height = 1280, 720
+size = width, height = 620, 650
 white = 255, 255, 255
 red = (255,0,0)
 black = (0,0,0)
@@ -11,8 +11,8 @@ x=0
 y=0
 screen = pygame.display.set_mode(size,pygame.RESIZABLE)
 #bfs.makemapa()
-#mapa = bfs.mapa
-camino = 0 #bfs.returnPath(2)
+mapa = bfs.mapa
+camino = bfs.returnPath(2)
 Mononoke = pygame.image.load("mononoke.png")
 Mononoke = pygame.transform.scale(Mononoke, (100, 100))
 Ciervo = pygame.image.load("ciervo.png")
@@ -21,48 +21,21 @@ Elemental = pygame.image.load("elementales.png")
 Elemental = pygame.transform.scale(Elemental,(100,100))
 dios_Ciervo = pygame.image.load("dios_ciervo.png")
 dios_Ciervo = pygame.transform.scale(dios_Ciervo,(100,100))
+Fuente = pygame.font.Font(None,20)
+Aviso_Amplitud= Fuente.render("Para Amplitud presione la tecla Q",True,(200,60,80))
+Aviso_CostoU= Fuente.render("Para Costo uniforme presione la tecla W",True,(135,200,80))
+Aviso_ProfundidadI= Fuente.render("Para Profundidad Iterativa presione la tecla E",True,(100,120,200))
 count = 0
 Xamongus = 0
 Yamongus = 0
 
-
-"""def Guiar_Mononoke(camino):
-    global Xamongus
-    global Yamongus
-    pintarmapa()
-    for i in range (len(camino)):
-        x1 = camino[i][0]
-        y1 = camino[i][1]
-        mapa[Xamongus][Yamongus] = "1"
-        mapa[x1][y1] ="5"
-        Xamongus = x1
-        Yamongus = y1
-        pintarmapa()
-        time.sleep(5)        """
-"""def Movimientos():
+"""def Eventos_teclado(camino):
     if event.type == pygame.KEYDOWN:
-        try:
-            contenido = mapa[(int(Xamongus)-1)][int(Yamongus)]
-            if contenido != "0"and not int(Xamongus) <=0 :
-                mapa[int(Xamongus)][int(Yamongus)]="1"
-                mapa[(int(Xamongus)-1)][int(Yamongus)]="5"
-            if event.key == pygame.K_DOWN:
-                contenido = mapa[(int(Xamongus)+1)][int(Yamongus)]
-                if contenido != "0" and not int(Xamongus) >= 3:
-                        mapa[int(Xamongus)][int(Yamongus)]="1"
-                        mapa[int(Xamongus)+1][int(Yamongus)]="5"
-            if event.key == pygame.K_LEFT:
-                contenido = mapa[(int(Xamongus))][int(Yamongus)-1]
-                if contenido != "0" and not int(Yamongus) <= 0:
-                        mapa[int(Xamongus)][int(Yamongus)]="1"
-                        mapa[int(Xamongus)][int(Yamongus)-1]="5"
-            if event.key == pygame.K_RIGHT:
-                contenido = mapa[(int(Xamongus))][int(Yamongus)+1]
-                if contenido != "0" and not int(Yamongus) >= 4:
-                        mapa[int(Xamongus)][int(Yamongus)]="1"
-                        mapa[int(Xamongus)][int(Yamongus)+1]="5"
-        except Exception as inst:
-            print("amongus no tiene mas espacio")"""
+        if event.key == pygame.K_q:
+            camino = bfs.returnPath(1)
+        if event.key == pygame.K_w:
+            camino = bfs.returnPath(2)
+    return camino"""
 
 def pintarmapa():
     x=0
@@ -109,10 +82,16 @@ def guiarMononoke():
 while 1:
     screen.fill(black)
     pintarmapa()
+    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+        #Eventos_teclado(camino)
     guiarMononoke()
-
     time.sleep(0.5)
+    screen.blit(Aviso_Amplitud,(40,510))
+    screen.blit(Aviso_CostoU,(40,550))
+    screen.blit(Aviso_ProfundidadI,(40,600))
+    
+
     pygame.display.flip()
